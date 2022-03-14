837828afe3ba00da9033b4a4ff0a425c7018b894
oscar.castillo <oscar.castillo@LPTOCASTILLO>
2022-03-14 15:16:44 -0500

    List Country

---
+++ Labora/Controllers/CountryController.cs
@@ -0,0 +1,25 @@
+﻿using Labora.Data.DataEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Labora.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly DataCountry _dataCountry = new DataCountry();
+        // GET: Contry
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ListCountry()
+        {
+            var resultado = _dataCountry.ListCountry();
+            return Json(resultado);
+        }
+    }
+}
